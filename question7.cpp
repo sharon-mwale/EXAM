a35ffa880cc1bfe283a080338a285045699e2c98
sharon mwale <sharoonmwale@gmail.com>
2023-02-26 06:02:11 -0800

    commit

--- question7.cpp
+++ question7.cpp
@@ -7,8 +7,8 @@ int main()
     int u;
     int a;
     int x;
-
-    std::pow(v, 2) = std::pow(u * 2) + 2 + a + x;
+    int v_pow = std::pow(v, 2);
+    v_pow = std::pow(u, 2) + 2 + a + x;
 
     return 0;
 }
